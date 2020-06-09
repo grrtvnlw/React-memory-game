815d39c31e465163a40b10ffa987f6d8a4418647
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-09 11:03:55 -0400

    Added click handler

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+  clickHandler() {
+    alert("card clicked")
+  }
+
   render() {
     return (
       <div>
-        <div className="MemoryCard">
+        <div className="MemoryCard" onClick={ this.clickHandler }>
           <div className="MemoryCard_inner">
             <div className="MemoryCard_back">
               <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png"></img>
