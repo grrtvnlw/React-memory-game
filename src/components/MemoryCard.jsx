1a9dc03aec0632b54bfaba7a6e0045f2166256df
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-09 11:23:14 -0400

    Click handler now functioning properly

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+
+  constructor() {
+    super()
+
+    this.state = { isFlipped: false };
+  }
+
   clickHandler() {
-    alert("card clicked")
+    this.setState({ isFlipped: !this.state.isFlipped })
   }
 
   render() {
+
+    let innerClass = 'MemoryCard_inner';
+
+    if (this.state.isFlipped) {
+      innerClass += ' flipped';
+    }
+
     return (
       <div>
-        <div className="MemoryCard" onClick={ this.clickHandler }>
-          <div className="MemoryCard_inner">
+        <div className="MemoryCard" onClick={ this.clickHandler.bind(this) }>
+          <div className={ innerClass }>
             <div className="MemoryCard_back">
               <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png"></img>
             </div>
